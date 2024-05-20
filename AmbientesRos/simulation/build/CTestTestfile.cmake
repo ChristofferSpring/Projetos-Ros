3e04f88f751deee0088a1b28ac3520ea22064bd3
ubuntu20 <ubuntu20@Ubuntu20.myguest.virtualbox.org>
2024-05-20 17:48:41 -0300

    simulation

---
+++ AmbientesRos/simulation/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu20/Desktop/simulation/src
+# Build directory: /home/ubuntu20/Desktop/simulation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_simulations")
+subdirs("two-wheeled-robot-simulation/my_worlds")
+subdirs("two-wheeled-robot-simulation/m2wr_description")
