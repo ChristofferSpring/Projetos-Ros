a1dccf15adc792272a57b369af64cc17a1d2e1db
ChristofferSpring <Christoffer@alunos.utfpr.edu.br>
2024-05-20 13:46:06 -0300

    finalizando comando basicos

--- AmbientesRos/Tutorial/catkin_ws/build/catkin_generated/order_packages.py
+++ AmbientesRos/Tutorial/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ubuntu20/projects/Projetos-Ros/AmbientesRos/Tutorial/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/ubuntu20/projects/Projetos-Ros/AmbientesRos/Tutorial/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu20/projects/Projetos-Ros/AmbientesRos/Tutorial/catkin_ws/devel;/opt/ros/noetic' != '' else []
